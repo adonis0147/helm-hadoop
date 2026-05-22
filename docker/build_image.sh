239dda57e2de79d1544db637c42f20502d5fdf12
Adonis Ling <adonis0147@gmail.com>
2026-05-23 00:09:08 +0800

    chore: bump the version of Hadoop

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -15,38 +15,38 @@ fi
 
 if [[ "${arch}" == 'x86_64' ]]; then
 	# shellcheck disable=2034
-	declare -r JAVA_URL="https://corretto.aws/downloads/resources/11.0.27.6.1/amazon-corretto-11.0.27.6.1-linux-${arch/86_/}.tar.gz"
+	declare -r JAVA_URL="https://corretto.aws/downloads/resources/17.0.19.10.1/amazon-corretto-17.0.19.10.1-linux-${arch/86_/}.tar.gz"
 	# shellcheck disable=2034
-	declare -r JAVA_SHA256SUM='0b4fd441b90471384af288ea7e927897114871c668ad292f4e982e7cb9f0cbf7'
+	declare -r JAVA_SHA512SUM='c2ec54f90191e99dc551e3c072d3ea9e4e20b938162bfbc79cd90ea1065e41705950998f89c134842c8957a8eab29a2a1539512f130d2e8f6370573af564bb7c'
 	# shellcheck disable=2034
-	declare -r HADOOP_URL='https://archive.apache.org/dist/hadoop/common/hadoop-3.4.3/hadoop-3.4.3.tar.gz'
+	declare -r HADOOP_URL='https://archive.apache.org/dist/hadoop/common/hadoop-3.5.0/hadoop-3.5.0.tar.gz'
 	# shellcheck disable=2034
-	declare -r HADOOP_SHA256SUM='ca65b67a9cdad27b3aa1cb81496a3136de572bab3e8f2289c2aade965f687353'
+	declare -r HADOOP_SHA512SUM='04ab94496cc00c8b7a28d03f6308eff8d2a4e7f37a9da5e8e086e4d6fc990e7a94d661908f6a6136039536efb362614b8aecdef185b5fb8ed588f0b152c7aa16'
 else
 	# shellcheck disable=2034
-	declare -r JAVA_URL="https://corretto.aws/downloads/resources/11.0.27.6.1/amazon-corretto-11.0.27.6.1-linux-${arch}.tar.gz"
+	declare -r JAVA_URL="https://corretto.aws/downloads/resources/17.0.19.10.1/amazon-corretto-17.0.19.10.1-linux-${arch}.tar.gz"
 	# shellcheck disable=2034
-	declare -r JAVA_SHA256SUM='679ab9f1f614d3ed000b61ccb5e0c06041c9ee29fb9c0ca1b598a9f23975cf85'
+	declare -r JAVA_SHA512SUM='b90dafc4aa4f1d4b26f7081330116b811e2ef3203cdaaf932ee8f7190a886e34cb49309395eceac1efc365312be3e113daa6eafc0e6dcfa594469608c1e7e279'
 	# shellcheck disable=2034
-	declare -r HADOOP_URL="https://archive.apache.org/dist/hadoop/common/hadoop-3.4.3/hadoop-3.4.3-${arch}.tar.gz"
+	declare -r HADOOP_URL="https://archive.apache.org/dist/hadoop/common/hadoop-3.5.0/hadoop-3.5.0-${arch}.tar.gz"
 	# shellcheck disable=2034
-	declare -r HADOOP_SHA256SUM='229016c37f7d1f2da43e9712e21236670c8cca3546df99e983e47d8a86860118'
+	declare -r HADOOP_SHA512SUM='feaa20fea4709050ee993aae23b05720383a0d65aeda97c68d042e52564e8e77a83196a5a6973abdc690a1e600a73308ade56ccf9baeec231d1cf0810703430f'
 fi
 
 function download() {
 	local package
 	package="$(echo "${1}" | awk '{print toupper($0)}')"
 	local url_variable="${package}_URL"
-	local checksum_variable="${package}_SHA256SUM"
+	local sha512sum_variable="${package}_SHA512SUM"
 
 	local url="${!url_variable}"
-	local checksum="${!checksum_variable}"
+	local checksum="${!sha512sum_variable}"
+	local checksum_command
+	checksum_command="$(command -v gsha512sum || command -v sha512sum)"
 	local filename
 	filename="$(basename "${url}")"
 
-	local SHA256SUM
-	SHA256SUM="$(command -v gsha256sum || true)"
-	if [[ ! -f "${filename}" ]] || ! echo "${checksum} ${filename}" | "${SHA256SUM:-sha256sum}" --check &>/dev/null; then
+	if [[ ! -f "${filename}" ]] || ! echo "${checksum} ${filename}" | "${checksum_command}" --check &>/dev/null; then
 		curl -LO "${url}"
 	fi
 }
